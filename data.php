2c8d841ed97d73a80eb3b45641afcc0c7af88855
yeferson <46008359+huajef@users.noreply.github.com>
2019-01-28 18:17:25 -0500

    Add files via upload

---
+++ data.php
@@ -0,0 +1,42 @@
+[
+   {
+      "title":"Manzana",
+      "description":"La manzana es el fruto del manzano, \u00e1rbol de la familia de las Ros\u00e1ceas. Esta familia incluye m\u00e1s de 2.000 especies de plantas herb\u00e1ceas, arbustos y \u00e1rboles distribuidos por regiones templadas de todo el mundo.",
+      "image":null,
+      "fecha":"01\/02\/2018",
+      "url":"http:\/\/frutas.consumer.es\/manzana\/propiedades",
+      "imageUrl":"http:\/\/frutas.consumer.es\/sites\/default\/files\/manzana.jpg"
+   },
+   {
+      "title":"Albaricoque",
+      "description":"El albaricoque, tambi\u00e9n conocido como damasco o alb\u00e9rchigo, es el fruto del albaricoquero, \u00e1rbol de la familia de las Ros\u00e1ceas. Esta familia incluye m\u00e1s de 2.000 especies de plantas herb\u00e1ceas, arbustos y \u00e1rboles distribuidos por regiones templadas de todo el mundo. Las principales frutas europeas, adem\u00e1s del rosal, pertenecen a esta gran familia. El fruto tiene la carne jugosa, firme y con un delicioso sabor dulce.",
+      "image":null,
+      "fecha":"01\/03\/2018",
+      "url":"http:\/\/frutas.consumer.es\/albaricoque\/propiedades",
+      "imageUrl":"http:\/\/frutas.consumer.es\/sites\/default\/files\/albaricoque.jpg"
+   },
+   {
+      "title":"Ar\u00e1ndano",
+      "description":"El ar\u00e1ndano es una baya que crece del peque\u00f1o arbusto hom\u00f3nimo de la familia de las Eric\u00e1ceas del g\u00e9nero Vaccinium, que alcanza de 25 a 50 cent\u00edmetros de altura. Este g\u00e9nero est\u00e1 formado por una docena de plantas que producen bayas de color oscuro, azuladas o rojizas, ricas en antocianos, pigmentos vegetales que les confieren su color caracter\u00edstico.",
+      "image":null,
+      "fecha":"02\/03\/2018",
+      "url":"http:\/\/frutas.consumer.es\/arandano\/propiedades",
+      "imageUrl":"http:\/\/frutas.consumer.es\/sites\/default\/files\/arandano.jpg"
+   },
+   {
+      "title":"Lima",
+      "description":"El lim\u00f3n y la lima son dos c\u00edtricos del g\u00e9nero Citrus que pertenecen a la familia de las Rut\u00e1ceas. Esta familia comprende m\u00e1s de 1.600 especies. El g\u00e9nero bot\u00e1nico Citrus es el m\u00e1s importante del grupo, y consta de unas 20 especies con frutos comestibles todos ellos muy abundantes en vitamina C, flavonoides y aceites esenciales. Los frutos, llamados hesp\u00e9rides, tienen la particularidad de que su endocarpio est\u00e1 formado por numerosas ves\u00edculas llenas de jugo.",
+      "image":null,
+      "fecha":"03\/03\/2018",
+      "url":"http:\/\/frutas.consumer.es\/lima\/propiedades",
+      "imageUrl":"http:\/\/frutas.consumer.es\/sites\/default\/files\/lima.jpg"
+   },
+   {
+      "title":"Pi\u00f1a",
+      "description":"La pi\u00f1a pertenece a la familia de las Bromeli\u00e1ceas, que comprende unas 1.400 especies de plantas, casi todas herb\u00e1ceas, de hoja perenne y con flores muy llamativas. Algunas de ellas producen enzimas proteol\u00edticas (que disgregan las prote\u00ednas de los alimentos) y se localizan sobre todo en Am\u00e9rica tropical.",
+      "image":null,
+      "fecha":"04\/03\/2018",
+      "url":"http:\/\/frutas.consumer.es\/pina\/propiedades",
+      "imageUrl":"http:\/\/frutas.consumer.es\/sites\/default\/files\/pina.jpg"
+   }
+]
